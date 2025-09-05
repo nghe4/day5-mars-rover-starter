6a614a6a4cb02760aebee1b08651a3b56e0c73ba
nghe4 <nghe4@oocl.com>
2025-09-05 16:09:32 +0800

    test: should_return_y_plus_when_input_command_M_direction_N

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -1,10 +1,19 @@
 package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_return_y_plus_when_input_command_M_direction_N() {
+        String command = "M";
+        String direction = "N";
+        int x = 0;
+        int y = 0;
 
+        MarsRover marsRover = new MarsRover(x, y, direction);
+        marsRover.executeCommand(command);
+
+        assertEquals(1, marsRover.getY());
     }
 }
