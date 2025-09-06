d58a954115b42694aab1176d776067ccb97e08ca
nghe4 <nghe4@oocl.com>
2025-09-06 17:35:47 +0800

    feat: add CommandFactory class

---
+++ src/test/java/com/afs/tdd/CommandFactory.java
@@ -0,0 +1,16 @@
+package com.afs.tdd;
+
+class CommandFactory {
+    public static Command createCommand(String commandType, MarsRover marsRover) {
+        switch (commandType) {
+            case "M":
+                return new MoveCommand(marsRover);
+            case "L":
+                return new TurnLeftCommand(marsRover);
+            case "R":
+                return new TurnRightCommand(marsRover);
+            default:
+                throw new IllegalArgumentException("Unknown command: " + commandType);
+        }
+    }
+}
